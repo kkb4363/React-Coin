9ac7900a0992e863386cc6eaac95cdf54f5c4517
gibeom <kkb4363@naver.com>
2022-10-11 09:09:56 +0900

    b

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,14 +1,14 @@
-import {HashRouter,BrowserRouter,Routes,Route} from 'react-router-dom';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
 import Coin from './Routes/Coin';
 import Coins from './Routes/Coins';
 
 function Router(){
-    return <HashRouter>
+    return <BrowserRouter basename="/Coin-App/">
     <Routes>
     <Route path='/:coinId/*' element={<Coin/>}/>
     <Route path='/' element={<Coins/>}/>
     </Routes>
-    </HashRouter>
+    </BrowserRouter>
 }
 
 export default Router;
